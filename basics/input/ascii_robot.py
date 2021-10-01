c786ca7d10530a0dbf1d9e8c6ddbb5eab94e49cc
Rodrigo Cruz Cazeiro Morgado Belo <5cruzr30@solent.ac.uk>
2021-10-01 15:45:54 +0100

    eyes

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,9 @@
+print("please enter a character for the eyes")
+name = input ()
+
+print("##########")
+print(f"#  {name}  {name}  #")
+print("#  ----  #")
+print("##########")
+print("\n")
+print("###############")
---
+++ basics/output/basics/user_input.py
@@ -0,0 +1,4 @@
+# ask user to enter their name
+print("What is your name human?")
+name = input ()
+print(f"It is nice to met you human {name}")
