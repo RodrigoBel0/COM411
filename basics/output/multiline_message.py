4a5775cf78a1b8ab5da4bc59cad2a92a14afa106
Rodrigo Cruz Cazeiro Morgado Belo <5cruzr30@solent.ac.uk>
2021-10-01 15:17:19 +0100

    escape chatacter

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,2 +1,8 @@
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hove engine")
+
+print("System online")
