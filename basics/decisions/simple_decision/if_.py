1eadb46100e122713d35e80b042382e888849584
Rodrigo Cruz Cazeiro Morgado Belo <5cruzr30@solent.ac.uk>
2021-10-08 14:40:58 +0100

    If code

---
+++ basics/decisions/simple_decision/if_.py
@@ -0,0 +1,8 @@
+#ask the user the type of book it is
+print("What type of book is this?")
+book = input()
+#if the book is adventure then
+if book == "adventure":
+    print("I like adventure books")
+
+print("Finished reading the book!")
---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,10 @@
+#ask user to enter activity
+print("Please enter the activity to be perfomed.")
+activity = input()
+#if
+if activity= "calculate":
+    print("Perfoming calculations...")
+else:
+    print("Perfoming activity...")
+
+print("Activity completed")
