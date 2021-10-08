d82f83b5ca21e5ac583003e76a0f42500919019d
Rodrigo Cruz Cazeiro Morgado Belo <5cruzr30@solent.ac.uk>
2021-10-08 15:42:42 +0100

    Added code to demonstrate how a variable can be used as a counter

---
+++ basics/decisions/simple_decision/comparasion_operators.py
@@ -0,0 +1,11 @@
+# enter numbers to find which is bigger
+print("Please enter the first number.")
+first_number = int(input())
+
+print("Please enter the second number.")
+second_number = int(input())
+
+if first_number > second_number:
+    print("The first number is the biggest.")
+else:
+    print("The second number is the biggest.")
