55ddbdaf4f84ce85fd34a21db5f3d9912b6912b5
Rodrigo Cruz Cazeiro Morgado Belo <5cruzr30@solent.ac.uk>
2021-10-08 15:21:43 +0100

    Addec code to demonstrate the use of comparasion operators

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,9 @@
+# modulo operator
+print("Please enter a whole number")
+# input number
+number= int(input())
+# if % is dividing x%2 it equal to 0 is even if not is odd
+if number % 2 == 0:
+    print(f"Number{number} is even.")
+else:
+    print(f"Number {number} is odd.")
